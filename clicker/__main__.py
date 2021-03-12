a4ab2fd37c15052f71fe191fd1d5348294f01c46
Felipe Muniz <muniz.r.felipe@gmail.com>
2021-03-12 01:34:52 -0300

    Include basic buildings in main loop

--- clicker/__main__.py
+++ clicker/__main__.py
@@ -1,2 +1,15 @@
+from clicker.building import Building
+
+
+def generate_buildings():
+    cursor = Building(batch=0.1, cost=15)
+    grandma = Building(batch=1, cost=100)
+    farm = Building(batch=8, cost=1.1e3)
+    mine = Building(batch=47, cost=12e3)
+    factory = Building(batch=260, cost=13e4)
+    bank = Building(batch=1.4e3, cost=1.4e6)
+    temple = Building(batch=7.8e3, cost=20e6)
+
+
 if __name__ == "__main__":
     pass
